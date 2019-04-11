14c509e3accba49e12b717cfd8b3580503d38fc0
Jeff Muizelaar <jrmuizel@gmail.com>
2019-04-10 22:52:37 -0400

    Get building after rebase

--- piet-coregraphics/src/lib.rs
+++ piet-coregraphics/src/lib.rs
@@ -1,20 +1,26 @@
 //! The CoreGraphics backend for the Piet 2D graphics abstraction.
 
 use core_graphics::context::{CGContext, CGLineCap, CGLineJoin};
+use core_graphics::image::CGImage;
+use core_graphics::base::{CGFloat};
 
-use kurbo::{PathEl, QuadBez, Vec2, Shape};
+use kurbo::{PathEl, Rect, Affine, QuadBez, Vec2, Shape};
 
-use piet::{RenderContext, RoundInto, FillRule};
+use piet::{
+    new_error, Error, ErrorKind, LineCap, LineJoin, FillRule, Font, FontBuilder, Gradient, ImageFormat,
+    InterpolationMode, RenderContext, RoundInto, StrokeStyle, Text, TextLayout, TextLayoutBuilder,
+};
 
 pub struct CoreGraphicsContext<'a> {
     // Cairo has this as Clone and with &self methods, but we do this to avoid
     // concurrency problems.
     ctx: &'a mut CGContext,
+    text: CoreGraphicsText,
 }
 
 impl<'a> CoreGraphicsContext<'a> {
     pub fn new(ctx: &mut CGContext) -> CoreGraphicsContext {
-        CoreGraphicsContext { ctx }
+        CoreGraphicsContext { ctx, text: CoreGraphicsText }
     }
 }
 
@@ -22,9 +28,64 @@ pub enum Brush {
     Solid(u32),
 }
 
+pub struct CoreGraphicsFont;
+impl Font for CoreGraphicsFont {
+}
+
+pub struct CoreGraphicsFontBuilder;
+
+impl FontBuilder for CoreGraphicsFontBuilder {
+    type Out = CoreGraphicsFont;
+
+        fn build(self) -> Result<Self::Out, Error> {
+panic!()
+        }
+}
+pub struct CoreGraphicsLayout;
+
+impl TextLayout for CoreGraphicsLayout {
+    type Coord = CGFloat;
+    fn width(&self) -> Self::Coord {
+        panic!();
+    }
+}
+
+pub struct CoreGraphicsTextLayoutBuilder {
+}
+
+impl TextLayoutBuilder for CoreGraphicsTextLayoutBuilder {
+    type Out = CoreGraphicsLayout;
+
+    fn build(self) -> Result<Self::Out, Error> {
+        panic!()
+    }
+}
+
+pub struct CoreGraphicsText;
+
+impl Text for CoreGraphicsText {
+    type Coord = CGFloat;
+    type FontBuilder = CoreGraphicsFontBuilder;
+    type Font = CoreGraphicsFont;
+    type TextLayoutBuilder = CoreGraphicsTextLayoutBuilder;
+    type TextLayout = CoreGraphicsLayout;
+
+    fn new_font_by_name(&mut self, name: &str, size: impl RoundInto<Self::Coord>,) -> Result<Self::FontBuilder, Error> {
+        panic!()
+    }
+
+       fn new_text_layout(
+        &mut self,
+        font: &Self::Font,
+        text: &str,
+    ) -> Result<Self::TextLayoutBuilder, Error> {
+        panic!()
+    }
+}
+
 // TODO: This cannot be used yet because the `piet::RenderContext` trait
 // needs to expose a way to create stroke styles.
-pub struct StrokeStyle {
+/*pub struct StrokeStyle {
     line_join: Option<CGLineJoin>,
     line_cap: Option<CGLineCap>,
     dash: Option<(Vec<f64>, f64)>,
@@ -60,13 +121,16 @@ impl StrokeStyle {
         self.miter_limit = Some(miter_limit);
         self
     }
-}
+}*/
 
 impl<'a> RenderContext for CoreGraphicsContext<'a> {
     type Point = Vec2;
-    type Coord = f64;
+    type Coord = f64;//XXX: this needs to be fixed for 32bit
     type Brush = Brush;
-    type StrokeStyle = StrokeStyle;
+    type Text = CoreGraphicsText;
+    type TextLayout = CoreGraphicsLayout;
+    type Image = CGImage;
+    //type StrokeStyle = StrokeStyle;
 
     fn clear(&mut self, rgb: u32) {
         self.ctx.set_rgb_fill_color(
@@ -78,13 +142,17 @@ impl<'a> RenderContext for CoreGraphicsContext<'a> {
         self.ctx.fill_rect(self.ctx.clip_bounding_box());
     }
 
-    fn solid_brush(&mut self, rgba: u32) -> Brush {
-        Brush::Solid(rgba)
+    fn solid_brush(&mut self, rgba: u32) -> Result<Brush, Error> {
+        Ok(Brush::Solid(rgba))
+    }
+
+    fn gradient(&mut self, gradient: Gradient) -> Result<Brush, Error> {
+        unimplemented!()
     }
 
     /// Fill a shape.
     fn fill(&mut self,
-        shape: &impl Shape,
+        shape: impl Shape,
         brush: &Self::Brush,
         fill_rule: FillRule,
     ) {
@@ -96,17 +164,95 @@ impl<'a> RenderContext for CoreGraphicsContext<'a> {
         }
     }
 
+    fn clip(&mut self, shape: impl Shape, fill_rule: FillRule) {
+        self.set_path(shape);
+        match fill_rule {
+            FillRule::NonZero => self.ctx.clip(),
+            FillRule::EvenOdd => self.ctx.eo_clip(),
+        }
+    }
+
     fn stroke(
         &mut self,
-        shape: &impl Shape,
+        shape: impl Shape,
         brush: &Self::Brush,
         width: impl RoundInto<Self::Coord>,
-        style: Option<&Self::StrokeStyle>,
+        style: Option<&StrokeStyle>,
     ) {
         self.set_path(shape);
         self.set_stroke(width.round_into(), style);
         self.set_stroke_brush(brush);
         self.ctx.stroke_path();
+    }
+
+    fn text(&mut self) -> &mut Self::Text {
+        &mut self.text
+    }
+
+    fn draw_text(
+        &mut self,
+        layout: &Self::TextLayout,
+        pos: impl RoundInto<Self::Point>,
+        brush: &Self::Brush,
+    ) {
+        unimplemented!()
+    }
+
+    fn save(&mut self) -> Result<(), Error> {
+        self.ctx.save();
+        Ok(())
+    }
+
+    fn restore(&mut self) -> Result<(), Error> {
+        self.ctx.restore();
+        Ok(())
+    }
+
+    fn finish(&mut self) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    fn transform(&mut self, transform: Affine) {
+        unimplemented!()
+    }
+
+    fn make_image(
+        &mut self,
+        width: usize,
+        height: usize,
+        buf: &[u8],
+        format: ImageFormat,
+    ) -> Result<Self::Image, Error> {
+        unimplemented!()
+    }
+
+    fn draw_image(
+                  &mut self,
+                  image: &Self::Image,
+                  rect: impl Into<Rect>,
+                  interp: InterpolationMode,
+                 ) {
+        unimplemented!()
+    }
+
+    fn status(&mut self) -> Result<(), Error> {
+        unimplemented!()
+    }
+}
+
+fn convert_line_join(line_join: LineJoin) -> CGLineJoin {
+    match line_join {
+        LineJoin::Miter => CGLineJoin::CGLineJoinMiter,
+        LineJoin::Round => CGLineJoin::CGLineJoinRound,
+        LineJoin::Bevel => CGLineJoin::CGLineJoinBevel,
+    }
+}
+
+fn convert_line_cap(line_cap: LineCap) -> CGLineCap {
+    match line_cap {
+        LineCap::Butt => CGLineCap::CGLineCapButt,
+        LineCap::Round => CGLineCap::CGLineCapRound,
+        LineCap::Square => CGLineCap::CGLineCapSquare,
     }
 }
 
@@ -143,13 +289,13 @@ impl<'a> CoreGraphicsContext<'a> {
 
         let line_join = style
             .and_then(|style| style.line_join)
-            .unwrap_or(CGLineJoin::CGLineJoinMiter);
-        self.ctx.set_line_join(line_join);
+            .unwrap_or(LineJoin::Miter);
+        self.ctx.set_line_join(convert_line_join(line_join));
 
         let line_cap = style
             .and_then(|style| style.line_cap)
-            .unwrap_or(CGLineCap::CGLineCapButt);
-        self.ctx.set_line_cap(line_cap);
+            .unwrap_or(LineCap::Butt);
+        self.ctx.set_line_cap(convert_line_cap(line_cap));
 
         let miter_limit = style.and_then(|style| style.miter_limit).unwrap_or(10.0);
         self.ctx.set_miter_limit(miter_limit);
@@ -160,7 +306,7 @@ impl<'a> CoreGraphicsContext<'a> {
         }
     }
 
-    fn set_path(&mut self, shape: &impl Shape) {
+    fn set_path(&mut self, shape: impl Shape) {
         // This shouldn't be necessary, we always leave the context in no-path
         // state. But just in case, and it should be harmless.
         self.ctx.begin_path();
@@ -190,6 +336,7 @@ impl<'a> CoreGraphicsContext<'a> {
             }
         }
     }
+
 }
 
 fn byte_to_frac(byte: u32) -> f64 {
