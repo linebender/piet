f073d158a430ae23ec2cd1db112ccb084d886186
Colin Rofls <colin@cmyr.net>
2020-05-11 14:55:34 -0400

    Move blurred rect code into piet::utils

--- piet/src/util.rs
+++ piet/src/util.rs
@@ -1,5 +1,7 @@
 //! Code useful for multiple backends
 
+use crate::kurbo::{Rect, Size};
+
 /// Counts the number of utf-16 code units in the given string.
 /// from xi-editor
 pub fn count_utf16(s: &str) -> usize {
@@ -36,6 +38,55 @@ pub fn count_until_utf16(s: &str, utf16_text_position: usize) -> Option<usize> {
     }
 
     None
+}
+
+/// Extent to which to expand the blur.
+const BLUR_EXTENT: f64 = 2.5;
+
+pub fn size_for_blurred_rect(rect: Rect, radius: f64) -> Size {
+    let padding = BLUR_EXTENT * radius;
+    let rect_padded = rect.inflate(padding, padding);
+    let rect_exp = rect_padded.expand();
+    rect_exp.size()
+}
+
+/// Generate image for a blurred rect, writing it into the provided buffer.
+pub fn compute_blurred_rect(rect: Rect, radius: f64, stride: usize, buf: &mut [u8]) -> Rect {
+    let radius_recip = radius.recip();
+    let xmax = rect.width() * radius_recip;
+    let ymax = rect.height() * radius_recip;
+    let padding = BLUR_EXTENT * radius;
+    let rect_padded = rect.inflate(padding, padding);
+    let rect_exp = rect_padded.expand();
+    let xfrac = rect_padded.x0 - rect_exp.x0;
+    let yfrac = rect_padded.y0 - rect_exp.y0;
+    let width = rect_exp.width() as usize;
+    let height = rect_exp.height() as usize;
+    let strip = (0..width)
+        .map(|i| {
+            let x = ((i as f64) - (xfrac + padding)) * radius_recip;
+            (255.0 * 0.25) * (compute_erf7(x) + compute_erf7(xmax - x))
+        })
+        .collect::<Vec<_>>();
+    {
+        for j in 0..height {
+            let y = ((j as f64) - (yfrac + padding)) * radius_recip;
+            let z = compute_erf7(y) + compute_erf7(ymax - y);
+            for i in 0..width {
+                buf[j * stride + i] = (z * strip[i]).round() as u8;
+            }
+        }
+    }
+    rect_exp
+}
+
+// See https://raphlinus.github.io/audio/2018/09/05/sigmoid.html for a little
+// explanation of this approximation to the erf function.
+fn compute_erf7(x: f64) -> f64 {
+    let x = x * std::f64::consts::FRAC_2_SQRT_PI;
+    let xx = x * x;
+    let x = x + (0.24295 + (0.03395 + 0.0104 * xx) * xx) * (x * xx);
+    x / (1.0 + x * x).sqrt()
 }
 
 #[cfg(test)]
