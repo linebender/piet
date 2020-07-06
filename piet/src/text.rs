c054bde43030ddda443ce0e39cfd4adb50558efe
Colin Rofls <colin@cmyr.net>
2020-07-06 16:36:25 -0400

    Add FontWeight

--- piet/src/text.rs
+++ piet/src/text.rs
@@ -31,6 +31,60 @@ pub trait FontBuilder {
 
 pub trait Font {}
 
+/// A font weight, represented as a value in the range 1..=1000.
+///
+/// This is based on the [CSS `font-weight`] property. In general, you should
+/// prefer the constants defined on this type, such as `FontWeight::REGULAR` or
+/// `FontWeight::BOLD`.
+///
+/// [CSS `font-weeight`]: https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct FontWeight(u16);
+
+impl FontWeight {
+    pub const THIN: FontWeight = FontWeight(100);
+    pub const HAIRLINE: FontWeight = FontWeight::THIN;
+
+    pub const EXTRA_LIGHT: FontWeight = FontWeight(200);
+
+    pub const LIGHT: FontWeight = FontWeight(300);
+
+    pub const REGULAR: FontWeight = FontWeight(400);
+    pub const NORMAL: FontWeight = FontWeight::REGULAR;
+
+    pub const MEDIUM: FontWeight = FontWeight(500);
+
+    pub const SEMI_BOLD: FontWeight = FontWeight(600);
+
+    pub const BOLD: FontWeight = FontWeight(700);
+
+    pub const EXTRA_BOLD: FontWeight = FontWeight(800);
+
+    pub const BLACK: FontWeight = FontWeight(900);
+    pub const HEAVY: FontWeight = FontWeight::BLACK;
+
+    pub const EXTRA_BLACK: FontWeight = FontWeight(950);
+
+    /// Create a new `FontWeight` with a custom value.
+    ///
+    /// Values will be clamped to the range 1..=1000.
+    pub fn new(raw: u16) -> FontWeight {
+        let raw = raw.min(1000).max(1);
+        FontWeight(raw)
+    }
+
+    /// Return the raw value as a u16.
+    pub const fn to_raw(self) -> u16 {
+        self.0
+    }
+}
+
+impl Default for FontWeight {
+    fn default() -> Self {
+        FontWeight::REGULAR
+    }
+}
+
 pub trait TextLayoutBuilder {
     type Out: TextLayout;
 
