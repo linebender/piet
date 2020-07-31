173ff5ee4e78ce6424f81926416c400221de096c
Colin Rofls <colin@cmyr.net>
2020-07-31 10:49:37 -0400

    Update sample drawing 7
    This makes the bounds of each column clear, to make it easier
    to see what's going on.

--- piet/src/samples/picture_7.rs
+++ piet/src/samples/picture_7.rs
@@ -1,13 +1,14 @@
 //! Text layouts
 
-use crate::kurbo::Size;
+use crate::kurbo::{Rect, Size};
 use crate::{Color, Error, RenderContext, Text, TextAlignment, TextAttribute, TextLayoutBuilder};
 
 pub const SIZE: Size = Size::new(800., 800.);
 static SAMPLE_EN: &str = r#"This essay is an effort to build an ironic political myth faithful to feminism, socialism, and materialism. Perhaps more faithful as blasphemy is faithful, than as reverent worship and identification. Blasphemy has always seemed to require taking things very seriously. I know no better stance to adopt from within the secular-religious, evangelical traditions of United States politics, including the politics of socialist-feminism."#;
 
-static SAMPLE_AR: &str = r#"لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار  النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور بالسعادة، ولكن بفضل هؤلاء الأشخاص الذين لا يدركون بأن السعادة لا بد أن نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة، وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو الألم ولكن نتيجة لظروف ما قد تكمن السعاده فيما نتحمله من كد وأسي.
-"#;
+static SAMPLE_AR: &str = r#"لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول استنكار  النشوة وتمجيد الألم نشأت بالفعل، وسأعرض لك التفاصيل لتكتشف حقيقة وأساس تلك السعادة البشرية، فلا أحد يرفض أو يكره أو يتجنب الشعور بالسعادة، ولكن بفضل هؤلاء الأشخاص الذين لا يدركون بأن السعادة لا بد أن نستشعرها بصورة أكثر عقلانية ومنطقية فيعرضهم هذا لمواجهة الظروف الأليمة، وأكرر بأنه لا يوجد من يرغب في الحب ونيل المنال ويتلذذ بالآلام، الألم هو الألم ولكن نتيجة لظروف ما قد تكمن السعاده فيما نتحمله من كد وأسي."#;
+
+const LIGHT_GREY: Color = Color::grey8(0xF0);
 
 pub fn draw<R: RenderContext>(rc: &mut R) -> Result<(), Error> {
     rc.clear(Color::WHITE);
@@ -68,6 +69,11 @@ pub fn draw<R: RenderContext>(rc: &mut R) -> Result<(), Error> {
         .max_width(100.0)
         .default_attribute(TextAttribute::Size(8.0))
         .build()?;
+
+    for pt in &[(0f64, 0f64), (200.0, 0.), (100., 200.), (300., 200.)] {
+        let rect = Rect::from_origin_size(*pt, (100., 200.));
+        rc.fill(rect, &LIGHT_GREY);
+    }
 
     rc.draw_text(&en_leading, (0., 0.));
     rc.draw_text(&en_trailing, (100., 0.));
