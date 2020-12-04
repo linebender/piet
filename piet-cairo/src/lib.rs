16a86dabd62dd06d71c0af86bffd77e2ae221c40
Colin Rofls <colin@cmyr.net>
2020-12-03 19:24:22 -0500

    Make cairo behave like otherr backends

--- piet-cairo/src/lib.rs
+++ piet-cairo/src/lib.rs
@@ -220,9 +220,8 @@ impl<'a> RenderContext for CairoRenderContext<'a> {
         format: ImageFormat,
     ) -> Result<Self::Image, Error> {
         let cairo_fmt = match format {
-            ImageFormat::Rgb => Format::Rgb24,
+            ImageFormat::Rgb | ImageFormat::Grayscale => Format::Rgb24,
             ImageFormat::RgbaSeparate | ImageFormat::RgbaPremul => Format::ARgb32,
-            ImageFormat::Grayscale => Format::A8,
             _ => return Err(Error::NotSupported),
         };
         let mut image = ImageSurface::create(cairo_fmt, width as i32, height as i32)
@@ -272,7 +271,9 @@ impl<'a> RenderContext for CairoRenderContext<'a> {
                     }
                     ImageFormat::Grayscale => {
                         for x in 0..width {
-                            data[dst_off + x] = 255 - buf[src_off + x];
+                            data[dst_off + x * 4 + 0] = buf[src_off + x];
+                            data[dst_off + x * 4 + 1] = buf[src_off + x];
+                            data[dst_off + x * 4 + 2] = buf[src_off + x];
                         }
                     }
                     _ => return Err(Error::NotSupported),
