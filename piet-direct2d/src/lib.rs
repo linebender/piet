97d51e985b99f863988d1c6f1c4a42b5ca3e4621
Richard Dodd <richard.o.dodd@gmail.com>
2020-12-22 18:01:15 +0000

    Add empty flag to `Bitmap`.

--- piet-direct2d/src/lib.rs
+++ piet-direct2d/src/lib.rs
@@ -334,12 +334,7 @@ impl<'a> RenderContext for D2DRenderContext<'a> {
         // empty image into 1x1 transparent image. Not ideal, but prevents a crash. TODO find a
         // better solution.
         if width == 0 || height == 0 {
-            return Ok(self.rt.create_bitmap(
-                1,
-                1,
-                &[0, 0, 0, 0][..],
-                D2D1_ALPHA_MODE_PREMULTIPLIED,
-            )?);
+            return Ok(self.rt.create_empty_bitmap()?);
         }
 
         // TODO: this method _really_ needs error checking, so much can go wrong...
@@ -544,8 +539,7 @@ fn draw_image<'a>(
     dst_rect: Rect,
     interp: InterpolationMode,
 ) {
-    let src_size = image.get_size();
-    if dst_rect.is_empty() || src_size.width == 0. || src_size.height == 0. {
+    if dst_rect.is_empty() || image.empty_image {
         // source or destination are empty
         return;
     }
@@ -578,7 +572,11 @@ impl<'a> IntoBrush<D2DRenderContext<'a>> for Brush {
 
 impl Image for Bitmap {
     fn size(&self) -> Size {
-        let inner = self.get_size();
-        Size::new(inner.width.into(), inner.height.into())
+        if self.empty_image {
+            Size::ZERO
+        } else {
+            let inner = self.get_size();
+            Size::new(inner.width.into(), inner.height.into())
+        }
     }
 }
