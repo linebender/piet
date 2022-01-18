5f31f7cce49eca0ded232fa89be625bb0aa1ee1a
Vitaliy Busko <vitaliy.opensource@gmail.com>
2022-01-18 13:06:04 +0700

    feat(piet): add Debug,Clone,PartialEq impls to TextAttribute

--- piet/src/text.rs
+++ piet/src/text.rs
@@ -141,6 +141,7 @@ impl std::ops::Deref for dyn TextStorage {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
 /// Attributes that can be applied to text.
 pub enum TextAttribute {
     /// The font family.
