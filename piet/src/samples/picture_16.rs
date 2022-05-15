867ea732be8c5092c373eb414432ecb000ced18f
Lucas Jenss <public@x3ro.de>
2022-05-15 16:09:42 +0200

    Fix `capture_image_area` not respecting CG transformation matrix
    The `capture_image_area` method of the CoreGraphicsContext was not
    taking into account that a transformation matrix is applied to the
    context. As a result, it only worked correctly if one applied this
    scaling manually to the `src_rect` prior to passing it to the method.
    
    With this addition, we now respect the CG transformation matrix and
    apply it to the `src_rect` prior to cropping the captured `CGImage`.

--- piet/src/samples/picture_16.rs
+++ piet/src/samples/picture_16.rs
@@ -4,18 +4,16 @@
 //!
 //! 1. clear ignores clipping and transforms
 
-use crate::kurbo::{Affine, Circle, Rect, Size};
+use crate::kurbo::{Rect, Size};
 use crate::{Color, Error, InterpolationMode, RenderContext};
 
 pub const SIZE: Size = Size::new(400., 400.);
 
 const RED: Color = Color::rgb8(255, 0, 0);
 const BLUE: Color = Color::rgb8(0, 0, 255);
-const SEMI_TRANSPARENT_GREEN: Color = Color::rgba8(0, 255, 0, 125);
-const SEMI_TRANSPARENT_WHITE: Color = Color::rgba8(255, 255, 255, 125);
 
 pub fn draw<R: RenderContext>(rc: &mut R) -> Result<(), Error> {
-    rc.clear(None, Color::BLACK);
+    rc.clear(None, Color::FUCHSIA);
 
     let outer_rect = Rect::new(20., 20., 180., 180.);
     let inner_rect = Rect::new(21., 21., 179., 179.);
@@ -25,14 +23,13 @@ pub fn draw<R: RenderContext>(rc: &mut R) -> Result<(), Error> {
     let bottom_left_corner = Rect::new(5., 150., 50., 195.);
     let bottom_right_corner = Rect::new(150., 150., 195., 195.);
 
-
     // Draw a box with a red border
     rc.fill(outer_rect, &RED);
     rc.fill(inner_rect, &BLUE);
 
     // Cache the box, clear the image and re-draw the box from the cache
     let cache = rc.capture_image_area(outer_rect).unwrap();
-    rc.clear(None, Color::WHITE);
+    rc.clear(None, Color::BLACK);
     rc.draw_image(&cache, outer_rect, InterpolationMode::NearestNeighbor);
 
     // Draw the cached image, scaled, in all four corners of the image
