d5b8da668d0df605adc9928ef15f615cc1f55630
Raph Levien <raph@google.com>
2024-12-06 15:11:42 -0800

    Rework tiling
    Simpler scalar code, not designed for thread parallelism.
    
    This renders tiger but is likely to have numerical robustness issues; the robustness logic from Vello has not been ported.

--- cpu-sparse/src/tiling.rs
+++ cpu-sparse/src/tiling.rs
@@ -29,6 +29,14 @@ pub struct Vec2 {
     pub y: f32,
 }
 
+const TILE_SCALE: f32 = 8192.0;
+// scale factor relative to unit square in tile
+const FRAC_TILE_SCALE: f32 = 8192.0 * 4.0;
+
+fn scale_up(z: f32) -> u32 {
+    (z * FRAC_TILE_SCALE).round() as u32
+}
+
 impl Vec2 {
     fn new(x: f32, y: f32) -> Self {
         Vec2 { x, y }
@@ -38,17 +46,18 @@ impl Vec2 {
         Vec2::new(xy[0], xy[1])
     }
 
+    #[allow(unused)]
     // Note: this assumes values in range.
     fn pack(self) -> u32 {
         // TODO: scale should depend on tile size
-        let x = (self.x * 8192.0).round() as u32;
-        let y = (self.y * 8192.0).round() as u32;
+        let x = (self.x * TILE_SCALE).round() as u32;
+        let y = (self.y * TILE_SCALE).round() as u32;
         (y << 16) + x
     }
 
     pub fn unpack(packed: u32) -> Self {
-        let x = (packed & 0xffff) as f32 * (1.0 / 8192.0);
-        let y = (packed >> 16) as f32 * (1.0 / 8192.0);
+        let x = (packed & 0xffff) as f32 * (1.0 / TILE_SCALE);
+        let y = (packed >> 16) as f32 * (1.0 / TILE_SCALE);
         Vec2::new(x, y)
     }
 }
@@ -81,151 +90,184 @@ fn span(a: f32, b: f32) -> u32 {
     (a.max(b).ceil() - a.min(b).floor()).max(1.0) as u32
 }
 
-const ONE_MINUS_ULP: f32 = 0.99999994;
-
-pub(crate) const ROBUST_EPSILON: f32 = 2e-7;
-
 pub fn make_tiles(lines: &[FlatLine], tile_buf: &mut Vec<Tile>) {
     tile_buf.clear();
     for line in lines {
         let p0 = Vec2::from_array(line.p0);
         let p1 = Vec2::from_array(line.p1);
-        let is_down = p1.y >= p0.y;
-        let (orig_xy0, orig_xy1) = if is_down { (p0, p1) } else { (p1, p0) };
-        let s0 = orig_xy0 * TILE_SCALE_X;
-        let s1 = orig_xy1 * TILE_SCALE_Y;
-        let count_x = span(s0.x, s1.x) - 1;
-        let count = count_x + span(s0.y, s1.y);
-
-        let dx = (s1.x - s0.x).abs();
-        let dy = s1.y - s0.y;
-        if dx + dy == 0.0 {
-            continue;
+        let s0 = p0 * TILE_SCALE_X;
+        let s1 = p1 * TILE_SCALE_Y;
+        let count_x = span(s0.x, s1.x);
+        let count_y = span(s0.y, s1.y);
+        let mut x = s0.x.floor();
+        if s0.x == x && s1.x < x {
+            // s0.x is on right side of first tile
+            x -= 1.0;
         }
-        if dy == 0.0 && s0.y.floor() == s0.y {
-            continue;
+        let mut y = s0.y.floor();
+        if s0.y == y && s1.y < y {
+            // s0.y is on bottom of first tile
+            y -= 1.0;
         }
-        let idxdy = 1.0 / (dx + dy);
-        let mut a = dx * idxdy;
-        let is_positive_slope = s1.x >= s0.x;
-        let sign = if is_positive_slope { 1.0 } else { -1.0 };
-        let xt0 = (s0.x * sign).floor();
-        let c = s0.x * sign - xt0;
-        let y0 = s0.y.floor();
-        let ytop = if s0.y == s1.y { s0.y.ceil() } else { y0 + 1.0 };
-        let b = ((dy * c + dx * (ytop - s0.y)) * idxdy).min(ONE_MINUS_ULP);
-        let robust_err = (a * (count as f32 - 1.0) + b).floor() - count_x as f32;
-        if robust_err != 0.0 {
-            a -= ROBUST_EPSILON.copysign(robust_err);
-        }
-        let x0 = xt0 * sign + if is_positive_slope { 0.0 } else { -1.0 };
-
-        let imin = 0;
-        let imax = count;
-        // In the Vello source, here's where we do clipping to viewport (by setting
-        // imin and imax to more restrictive values).
-        // Note: we don't really need to compute this if imin == 0, but it's cheap
-        let mut last_z = (a * (imin as f32 - 1.0) + b).floor();
-        for i in imin..imax {
-            let zf = a * i as f32 + b;
-            let z = zf.floor();
-            let y = (y0 + i as f32 - z) as i32;
-            let x = (x0 + sign * z) as i32;
-
-            let tile_xy = Vec2::new(x as f32 * TILE_WIDTH as f32, y as f32 * TILE_HEIGHT as f32);
-            let tile_xy1 = tile_xy + Vec2::new(TILE_WIDTH as f32, TILE_HEIGHT as f32);
-
-            let mut xy0 = orig_xy0;
-            let mut xy1 = orig_xy1;
-            if i > 0 {
-                if z == last_z {
-                    // Top edge is clipped
-                    // This calculation should arguably be done on orig_xy. Also might
-                    // be worth retaining slope.
-                    let mut xt = xy0.x + (xy1.x - xy0.x) * (tile_xy.y - xy0.y) / (xy1.y - xy0.y);
-                    xt = xt.clamp(tile_xy.x + 1e-3, tile_xy1.x);
-                    xy0 = Vec2::new(xt, tile_xy.y);
+        let xfrac0 = scale_up(s0.x - x);
+        let yfrac0 = scale_up(s0.y - y);
+        let packed0 = (yfrac0 << 16) + xfrac0;
+        // These could be replaced with <2 and the max(1.0) in span removed
+        if count_x == 1 {
+            let xfrac1 = scale_up(s1.x - x);
+            if count_y == 1 {
+                let yfrac1 = scale_up(s1.y - y);
+                let packed1 = (yfrac1 << 16) + xfrac1;
+                // 1x1 tile
+                tile_buf.push(Tile {
+                    x: x as u16,
+                    y: y as u16,
+                    p0: packed0,
+                    p1: packed1,
+                });
+            } else {
+                // vertical column
+                let slope = (s1.x - s0.x) / (s1.y - s0.y);
+                let sign = (s1.y - s0.y).signum();
+                let mut xclip0 = (s0.x - x) + (y - s0.y) * slope;
+                let yclip = if sign > 0.0 {
+                    xclip0 += slope;
+                    scale_up(1.0)
                 } else {
-                    // If is_positive_slope, left edge is clipped, otherwise right
-                    let x_clip = if is_positive_slope {
-                        tile_xy.x
-                    } else {
-                        tile_xy1.x
-                    };
-                    let mut yt = xy0.y + (xy1.y - xy0.y) * (x_clip - xy0.x) / (xy1.x - xy0.x);
-                    yt = yt.clamp(tile_xy.y + 1e-3, tile_xy1.y);
-                    xy0 = Vec2::new(x_clip, yt);
+                    0
+                };
+                let mut last_packed = packed0;
+                for i in 0..count_y - 1 {
+                    let xclip = xclip0 + i as f32 * sign * slope;
+                    let xfrac = scale_up(xclip).max(1);
+                    let packed = (yclip << 16) + xfrac;
+                    tile_buf.push(Tile {
+                        x: x as u16,
+                        y: (y + i as f32 * sign) as u16,
+                        p0: last_packed,
+                        p1: packed,
+                    });
+                    // flip y between top and bottom of tile
+                    last_packed = packed ^ ((FRAC_TILE_SCALE as u32) << 16);
                 }
+                let yfrac1 = scale_up(s1.y - (y + (count_y - 1) as f32 * sign));
+                let packed1 = (yfrac1 << 16) + xfrac1;
+
+                tile_buf.push(Tile {
+                    x: x as u16,
+                    y: (y + (count_y - 1) as f32 * sign) as u16,
+                    p0: last_packed,
+                    p1: packed1,
+                });
             }
-            if i < count - 1 {
-                let z_next = (a * (i as f32 + 1.0) + b).floor();
-                if z == z_next {
-                    // Bottom edge is clipped
-                    let mut xt = xy0.x + (xy1.x - xy0.x) * (tile_xy1.y - xy0.y) / (xy1.y - xy0.y);
-                    xt = xt.clamp(tile_xy.x + 1e-3, tile_xy1.x);
-                    xy1 = Vec2::new(xt, tile_xy1.y);
-                } else {
-                    // If is_positive_slope, right edge is clipped, otherwise left
-                    let x_clip = if is_positive_slope {
-                        tile_xy1.x
-                    } else {
-                        tile_xy.x
-                    };
-                    let mut yt = xy0.y + (xy1.y - xy0.y) * (x_clip - xy0.x) / (xy1.x - xy0.x);
-                    yt = yt.clamp(tile_xy.y + 1e-3, tile_xy1.y);
-                    xy1 = Vec2::new(x_clip, yt);
-                }
-            }
-            // Apply numerical robustness logic
-            let mut p0 = xy0 - tile_xy;
-            let mut p1 = xy1 - tile_xy;
-            // one count in fixed point
-            const EPSILON: f32 = 1.0 / 8192.0;
-            if p0.x < EPSILON {
-                if p1.x < EPSILON {
-                    p0.x = EPSILON;
-                    if p0.y < EPSILON {
-                        // Entire tile
-                        p1.x = EPSILON;
-                        p1.y = TILE_HEIGHT as f32;
-                    } else {
-                        // Make segment disappear
-                        p1.x = 2.0 * EPSILON;
-                        p1.y = p0.y;
-                    }
-                } else if p0.y < EPSILON {
-                    p0.x = EPSILON;
-                }
-            } else if p1.x < EPSILON {
-                if p1.y < EPSILON {
-                    p1.x = EPSILON;
-                }
-            }
-            // Question: do we need these? Also, maybe should be post-rounding?
-            if p0.x == p0.x.floor() && p0.x != 0.0 {
-                p0.x -= EPSILON;
-            }
-            if p1.x == p1.x.floor() && p1.x != 0.0 {
-                p1.x -= EPSILON;
-            }
-            if !is_down {
-                (p0, p1) = (p1, p0);
-            }
-            // These are regular asserts in Vello, but are debug asserts
-            // here for performance reasons.
-            debug_assert!(p0.x >= 0.0 && p0.x <= TILE_WIDTH as f32);
-            debug_assert!(p0.y >= 0.0 && p0.y <= TILE_HEIGHT as f32);
-            debug_assert!(p1.x >= 0.0 && p1.x <= TILE_WIDTH as f32);
-            debug_assert!(p1.y >= 0.0 && p1.y <= TILE_HEIGHT as f32);
-            let tile = Tile {
-                x: x as u16,
-                y: y as u16,
-                p0: p0.pack(),
-                p1: p1.pack(),
+        } else if count_y == 1 {
+            // horizontal row
+            let slope = (s1.y - s0.y) / (s1.x - s0.x);
+            let sign = (s1.x - s0.x).signum();
+            let mut yclip0 = (s0.y - y) + (x - s0.x) * slope;
+            let xclip = if sign > 0.0 {
+                yclip0 += slope;
+                scale_up(1.0)
+            } else {
+                0
             };
-            tile_buf.push(tile);
+            let mut last_packed = packed0;
+            for i in 0..count_x - 1 {
+                let yclip = yclip0 + i as f32 * sign * slope;
+                let yfrac = scale_up(yclip).max(1);
+                let packed = (yfrac << 16) + xclip;
+                tile_buf.push(Tile {
+                    x: (x + i as f32 * sign) as u16,
+                    y: y as u16,
+                    p0: last_packed,
+                    p1: packed,
+                });
+                // flip x between left and right of tile
+                last_packed = packed ^ (FRAC_TILE_SCALE as u32);
+            }
+            let xfrac1 = scale_up(s1.x - (x + (count_x - 1) as f32 * sign));
+            let yfrac1 = scale_up(s1.y - y);
+            let packed1 = (yfrac1 << 16) + xfrac1;
 
-            last_z = z;
+            tile_buf.push(Tile {
+                x: (x + (count_x - 1) as f32 * sign) as u16,
+                y: y as u16,
+                p0: last_packed,
+                p1: packed1,
+            });
+        } else {
+            // general case
+            let recip_dx = 1.0 / (s1.x - s0.x);
+            let signx = (s1.x - s0.x).signum();
+            let recip_dy = 1.0 / (s1.y - s0.y);
+            let signy = (s1.y - s0.y).signum();
+            // t parameter for next intesection with a vertical grid line
+            let mut t_clipx = (x - s0.x) * recip_dx;
+            let xclip = if signx > 0.0 {
+                t_clipx += recip_dx;
+                scale_up(1.0)
+            } else {
+                0
+            };
+            // t parameter for next intesection with a horizontal grid line
+            let mut t_clipy = (y - s0.y) * recip_dy;
+            let yclip = if signy > 0.0 {
+                t_clipy += recip_dy;
+                scale_up(1.0)
+            } else {
+                0
+            };
+            let x1 = x + (count_x - 1) as f32 * signx;
+            let y1 = y + (count_y - 1) as f32 * signy;
+            let mut xi = x;
+            let mut yi = y;
+            let mut last_packed = packed0;
+            let mut count = 0;
+            while xi != x1 || yi != y1 {
+                count += 1;
+                if count == 400 {
+                    panic!();
+                }
+                if t_clipy < t_clipx {
+                    // intersected with horizontal grid line
+                    let x_intersect = s0.x + (s1.x - s0.x) * t_clipy - xi;
+                    let xfrac = scale_up(x_intersect).max(1); // maybe should clamp?
+                    let packed = (yclip << 16) + xfrac;
+                    tile_buf.push(Tile {
+                        x: xi as u16,
+                        y: yi as u16,
+                        p0: last_packed,
+                        p1: packed,
+                    });
+                    t_clipy += recip_dy.abs();
+                    yi += signy;
+                    last_packed = packed ^ ((FRAC_TILE_SCALE as u32) << 16);
+                } else {
+                    // intersected with vertical grid line
+                    let y_intersect = s0.y + (s1.y - s0.y) * t_clipx - yi;
+                    let yfrac = scale_up(y_intersect).max(1); // maybe should clamp?
+                    let packed = (yfrac << 16) + xclip;
+                    tile_buf.push(Tile {
+                        x: xi as u16,
+                        y: yi as u16,
+                        p0: last_packed,
+                        p1: packed,
+                    });
+                    t_clipx += recip_dx.abs();
+                    xi += signx;
+                    last_packed = packed ^ (FRAC_TILE_SCALE as u32);
+                }
+            }
+            let xfrac1 = scale_up(s1.x - xi);
+            let yfrac1 = scale_up(s1.y - yi);
+            let packed1 = (yfrac1 << 16) + xfrac1;
+
+            tile_buf.push(Tile {
+                x: xi as u16,
+                y: yi as u16,
+                p0: last_packed,
+                p1: packed1,
+            });
         }
     }
     // This particular choice of sentinel tiles generates a sentinel strip.
@@ -241,4 +283,22 @@ pub fn make_tiles(lines: &[FlatLine], tile_buf: &mut Vec<Tile>) {
         p0: 0,
         p1: 0,
     });
+}
+
+#[test]
+fn tiling() {
+    let l = FlatLine {
+        p0: [1.3, 1.4],
+        p1: [20.1, 50.2],
+    };
+    let mut tiles = vec![];
+    make_tiles(&[l], &mut tiles);
+    for tile in &tiles {
+        let p0 = Vec2::unpack(tile.p0);
+        let p1 = Vec2::unpack(tile.p1);
+        println!(
+            "@{}, {}: ({}, {}) - ({}, {})",
+            tile.x, tile.y, p0.x, p0.y, p1.x, p1.y
+        );
+    }
 }
